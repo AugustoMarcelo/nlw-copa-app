45fb117ad7cb2cb19a56aa132260e638975dcfca
Marcelo <mrclgst10@gmail.com>
2022-11-02 13:56:22 -0300

    configure native-base, theme, fonts and splashscreen

---
+++ App.tsx
@@ -0,0 +1,38 @@
+import {
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { StyleSheet } from 'react-native';
+import { Loading } from './src/components/Loading';
+import { SignIn } from './src/screens/SignIn';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  return (
+    <NativeBaseProvider>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Loading /> : <SignIn />}
+    </NativeBaseProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
