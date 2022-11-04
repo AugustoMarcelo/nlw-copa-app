3bf8b286ce79dc80649373f24c27dce2417df0d7
Marcelo <mrclgst10@gmail.com>
2022-11-04 09:25:54 -0300

    create Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,34 @@
+import { Button as ButtonNativeBase, IButtonProps, Text } from 'native-base';
+
+interface Props extends IButtonProps {
+  title: string;
+  type?: 'primary' | 'secondary';
+}
+
+export function Button({ title, type = 'primary', ...rest }: Props) {
+  return (
+    <ButtonNativeBase
+      w="full"
+      h={14}
+      rounded="sm"
+      fontSize="md"
+      textTransform="uppercase"
+      bg={type === 'primary' ? 'yellow.500' : 'red.500'}
+      _pressed={{
+        bg: type === 'primary' ? 'yellow.600' : 'red.600',
+      }}
+      _loading={{
+        _spinner: { color: 'black' },
+      }}
+      {...rest}
+    >
+      <Text
+        fontSize="sm"
+        fontFamily="heading"
+        color={type === 'primary' ? 'black' : 'white'}
+      >
+        {title}
+      </Text>
+    </ButtonNativeBase>
+  );
+}
