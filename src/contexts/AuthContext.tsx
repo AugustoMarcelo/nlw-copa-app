8fd0bc867b773f13f08a7a05abb04f96ff0edde6
Marcelo <mrclgst10@gmail.com>
2022-11-04 14:13:14 -0300

    implement oauth with google and get the token

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,9 @@
-import { createContext, ReactNode } from 'react';
+import { makeRedirectUri } from 'expo-auth-session';
+import { useAuthRequest } from 'expo-auth-session/build/providers/Google';
+import { maybeCompleteAuthSession } from 'expo-web-browser';
+import { createContext, ReactNode, useEffect, useState } from 'react';
+
+maybeCompleteAuthSession();
 
 interface UserProps {
   name: string;
@@ -7,6 +12,7 @@ interface UserProps {
 
 export interface AuthContextDataProps {
   user: UserProps;
+  isUserLoading: Boolean;
   signIn: () => Promise<void>;
 }
 
@@ -17,18 +23,44 @@ interface AuthProviderProps {
 export const AuthContext = createContext({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<UserProps>({} as UserProps);
+  const [isUserLoading, setIsUserLoading] = useState(false);
+
+  const [request, response, promptAsync] = useAuthRequest({
+    clientId:
+      '1071186759854-s6mqhtkpcjkb53dgrmcg41jhj5qaa1mi.apps.googleusercontent.com',
+    redirectUri: makeRedirectUri({ useProxy: true }),
+    scopes: ['profile', 'email'],
+  });
+
   async function signIn() {
-    console.log('Sign In');
+    try {
+      setIsUserLoading(true);
+      await promptAsync();
+    } catch (error) {
+      console.log(error);
+      throw error;
+    } finally {
+      setIsUserLoading(false);
+    }
   }
+
+  async function signInWithGoogle(access_token: string) {
+    console.log(`Authentication token: ${access_token}`);
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success' && response.authentication.accessToken) {
+      signInWithGoogle(response.authentication.accessToken);
+    }
+  }, [response]);
 
   return (
     <AuthContext.Provider
       value={{
         signIn,
-        user: {
-          name: 'Marcelo',
-          avatarUrl: 'https://github.com/augustomarcelo.png',
-        },
+        isUserLoading,
+        user,
       }}
     >
       {children}
