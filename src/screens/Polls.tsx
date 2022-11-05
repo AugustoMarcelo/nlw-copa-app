1cc84a0635dad891af50a2e692fd31674e170482
Marcelo <mrclgst10@gmail.com>
2022-11-05 15:49:01 -0300

    implement my polls list

--- src/screens/Polls.tsx
+++ src/screens/Polls.tsx
@@ -1,11 +1,45 @@
 import { Octicons } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
-import { Icon, VStack } from 'native-base';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
+import { FlatList, Icon, useToast, VStack } from 'native-base';
 import { Button } from '../components/Button';
 import { Header } from '../components/Header';
+import { api } from '../services/api';
+
+import { useCallback, useState } from 'react';
+import { EmptyPollList } from '../components/EmptyPollList';
+import { Loading } from '../components/Loading';
+import { PollCard, PollCardProps } from '../components/PollCard';
 
 export function Polls() {
+  const [isPollsLoading, setIsPollsLoading] = useState(false);
+  const [polls, setPolls] = useState<PollCardProps[]>([]);
+
   const navigation = useNavigation();
+  const toast = useToast();
+
+  async function fetchPolls() {
+    try {
+      setIsPollsLoading(true);
+
+      const pollsResponse = await api.get('/polls');
+
+      setPolls(pollsResponse.data.polls);
+    } catch (error) {
+      toast.show({
+        title: 'It was not possible to show the polls.',
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    } finally {
+      setIsPollsLoading(false);
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchPolls();
+    }, [])
+  );
 
   return (
     <VStack flex={1} bgColor="gray.900">
@@ -26,6 +60,20 @@ export function Polls() {
           onPress={() => navigation.navigate('FindPoll')}
         />
       </VStack>
+
+      {isPollsLoading ? (
+        <Loading />
+      ) : (
+        <FlatList
+          data={polls}
+          keyExtractor={(item) => item.id}
+          px={5}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ pb: 10 }}
+          ListEmptyComponent={() => <EmptyPollList />}
+          renderItem={({ item }) => <PollCard data={item} />}
+        />
+      )}
     </VStack>
   );
 }
