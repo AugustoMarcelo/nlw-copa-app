311a8d9eeacc3b48274d9d3f87056f8579e063c4
Marcelo <mrclgst10@gmail.com>
2022-11-05 14:54:06 -0300

    create type to define available routes in the app

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,12 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      CreatePoll: undefined;
+      Polls: undefined;
+      FindPoll: undefined;
+      PollDetails: {
+        id: string;
+      };
+    }
+  }
+}
